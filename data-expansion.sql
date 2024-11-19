2f1a91c3f0532adc1544d554304c5ea08e5fa2d1
nkali <kalinurbah040508@gmail.com>
2024-11-19 13:47:26 +0500

    domain expansion

---
+++ data-expansion.sql
@@ -0,0 +1,6 @@
+INSERT INTO students (student_id, first_name, last_name, email, enrollment_date)
+VALUES 
+(4, 'Charlotte', 'Montgomery', 'charlotte.montgomery@gmail.com', '2022-11-15'),
+(5, 'Edmond', 'Dantes', 'edmond.dantes@grafmontecristo.fr', '1828-09-15'),
+(6, 'Mercedes', 'Herrera', 'mercedes.herrera@grafmontecristo.fr', '1827-08-20'),
+(7, 'Albert', 'Einstein', 'albert.einstein@relativity.org', '1905-03-14');

498a4a4d1c0f11166a338bcbaeed4c5a79698d1e
nkali <kalinurbah040508@gmail.com>
2024-11-19 20:06:36 +0500

    cap

--- math-master.sql
+++ math-master.sql
@@ -1,7 +1,7 @@
 select
 name,
 abs(quantity-100) as quantity_diff,
-round(price),
+round(price) as rounded_price,
 rating * 20 as rating_percent,
 case when price > 50 then price else 50 end as price_or_50,
 case when quantity < 200 then 200 else quantity end as quantity_cap,

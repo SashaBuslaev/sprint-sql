407732905d6937650f4e8a18fef0e499ed669737
nkali <kalinurbah040508@gmail.com>
2024-11-19 13:26:47 +0500

    new table

--- data-drop-in.sql
+++ data-drop-in.sql
@@ -1,6 +1,6 @@
 
-INSERT INTO students (first_name, last_name, email, enrollment_date)
+INSERT INTO students (student_id, first_name, last_name, email, enrollment_date)
 VALUES 
-('Mustafa', 'Sarvarov', 'mustafa.sarvarov@gmail.com', '2023-10-04'),
-('Arthur', 'Kim', 'arthur.kim@mail.ru', '2024-10-04'),
-('Elnar', 'Moldabekov', 'elnar.moldabekov', '2023-01-16');
+(1, 'Mustafa', 'Sarvarov', 'mustafa.sarvarov@gmail.com', '2023-10-04'),
+(2, 'Arthur', 'Kim', 'arthur.kim@mail.ru', '2024-10-04'),
+(3, 'Elnar', 'Moldabekov', 'elnar.moldabekov', '2023-01-16');

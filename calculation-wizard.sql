6e59b84201d73dd5493a19cf2463c57807098c08
nkali <kalinurbah040508@gmail.com>
2024-11-19 19:42:25 +0500

    comma

--- calculation-wizard.sql
+++ calculation-wizard.sql
@@ -2,6 +2,6 @@ select
 first_name || ' ' || last_name as full_name, 
 (salary * 12) as annual_salary,
 salary * 1.1 as increased_salary,
-DATEDIFF('2024-11-19', hire_date) / 365.25 AS years_employed
+DATEDIFF('2024-11-19', hire_date) / 365.25 AS years_employed,
 salary / 20 as daily_rate
 from employees;

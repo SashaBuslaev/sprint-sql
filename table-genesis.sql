59c4c99c30b6845b70d18eb9ec802c6be30fb032
nkali <kalinurbah040508@gmail.com>
2024-11-19 13:13:18 +0500

    tennis

--- table-genesis.sql
+++ table-genesis.sql
@@ -2,6 +2,6 @@ CREATE TABLE students (
     student_id int PRIMARY KEY,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
-    email varchar(100) PRIMARY KEY,
+    email varchar(100) NOT NULL UNIQUE,
     enrollment_date date NOT NULL
 );

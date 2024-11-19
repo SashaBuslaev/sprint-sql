1a89e2a0c876c6d62346a3b910cf0fe3bb5e638e
nkali <kalinurbah040508@gmail.com>
2024-11-19 18:40:54 +0500

    alias

---
+++ column-commander.sql
@@ -0,0 +1,5 @@
+SELECT first_name, last_name from employees
+where department_id = 1;
+select email contact_info, position job_title from employees
+where position = "Senior Engineer";
+select first_name given_name, last_name, salary monthly_income from employees; 

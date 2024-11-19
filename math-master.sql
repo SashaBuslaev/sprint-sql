a300250659e1def734cb1a3953f7a5f5b4d92344
nkali <kalinurbah040508@gmail.com>
2024-11-19 20:09:18 +0500

    200

--- math-master.sql
+++ math-master.sql
@@ -4,6 +4,6 @@ abs(quantity-100) as quantity_diff,
 round(price) as rounded_price,
 rating * 20 as rating_percent,
 case when price > 50 then price else 50 end as price_or_50,
-case when quantity < 200 then 200 else quantity end as quantity_cap,
+case when quantity < 200 then quantity else 200 end as quantity_cap,
 price * quantity as total_value
 from products;

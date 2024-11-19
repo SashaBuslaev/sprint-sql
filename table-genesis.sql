3d5363c43169ab0d77540aa4e7fda9823997d20e
nkali <kalinurbah040508@gmail.com>
2024-11-19 13:23:49 +0500

    tennis

--- table-genesis.sql
+++ table-genesis.sql
@@ -5,3 +5,9 @@ CREATE TABLE students (
     email varchar(100) NOT NULL UNIQUE,
     enrollment_date date NOT NULL
 );
+
+INSERT INTO student (first_name, last_name, email, enrollment_date)
+VALUES 
+('Mustafa', 'Sarvarov', 'mustafa.sarvarov@gmail.com', '2023-10-04'),
+('Arthur', 'Kim', 'arthur.kim@mail.ru', '2024-10-04'),
+('Elnar', 'Moldabekov', 'elnar.moldabekov', '2023-01-16');

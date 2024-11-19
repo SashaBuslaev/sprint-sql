5930b8814c7139489fb874bc587ea561b895c77c
nkali <kalinurbah040508@gmail.com>
2024-11-19 20:22:26 +0500

    first last

--- where-warrior.sql
+++ where-warrior.sql
@@ -1,4 +1,4 @@
-select last_name, first_name, salary from employees where salary > 55000;
-select last_name, first_name, salary from employees where hire_date > '2022-06-01';
-select last_name, first_name, salary from employees where salary > 50000 and department_id = 2;
-select last_name, first_name, position from employees where instr(position, 'Lead');
+select first_name, last_name, salary from employees where salary > 55000;
+select first_name, last_name, salary from employees where hire_date > '2022-06-01';
+select first_name, last_name, salary from employees where salary < 50000 and department_id = 2;
+select first_name, last_name, position from employees where instr(position, 'Lead');

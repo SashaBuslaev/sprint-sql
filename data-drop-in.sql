f2b68bcb000daf8424e1daaed4365948b2bf3669
nkali <kalinurbah040508@gmail.com>
2024-11-19 13:25:33 +0500

    tennis

--- data-drop-in.sql
+++ data-drop-in.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO student (first_name, last_name, email, enrollment_date)
+INSERT INTO students (first_name, last_name, email, enrollment_date)
 VALUES 
 ('Mustafa', 'Sarvarov', 'mustafa.sarvarov@gmail.com', '2023-10-04'),
 ('Arthur', 'Kim', 'arthur.kim@mail.ru', '2024-10-04'),

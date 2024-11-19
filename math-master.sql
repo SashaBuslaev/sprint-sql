3bbb51328c7846b1c3347d401de4f7200b7fe96b
nkali <kalinurbah040508@gmail.com>
2024-11-19 20:05:55 +0500

    min max

--- math-master.sql
+++ math-master.sql
@@ -3,7 +3,7 @@ name,
 abs(quantity-100) as quantity_diff,
 round(price),
 rating * 20 as rating_percent,
-case when max(price) > 50 then max(price) else 50 end as price_or_50,
-case when min(quantity) < 200 then 200 else min(quantity) end as quantity_cap,
+case when price > 50 then price else 50 end as price_or_50,
+case when quantity < 200 then 200 else quantity end as quantity_cap,
 price * quantity as total_value
 from products;

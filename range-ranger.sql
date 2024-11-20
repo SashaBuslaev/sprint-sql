776280d9f58d385ca27a1ff6be934061e2c48bac
nkali <kalinurbah040508@gmail.com>
2024-11-20 13:37:16 +0500

    hire_date

--- range-ranger.sql
+++ range-ranger.sql
@@ -1,8 +1,8 @@
-select first_name, last_name, salary, position, department_id from employees 
+select first_name, last_name, hire_date, position, department_id from employees 
 where salary between 45000 and 55000;
-select first_name, last_name, salary, position, department_id from employees 
+select first_name, last_name, hire_date, position, department_id from employees 
 where department_id in (1, 3);
 select first_name, last_name, hire_date, position, department_id from employees 
 where hire_date  between '2014-01-01' and '2019-12-31';
-select first_name, last_name, salary, position, department_id from employees 
+select first_name, last_name, hire_date, position, department_id from employees 
 where position in ('Warehouse Manager', 'Senior Researcher', 'Lead Engineer');

95c647f92f300560a2c8b368323b37070c57c479
nkali <kalinurbah040508@gmail.com>
2024-11-20 13:20:52 +0500

    strftime

--- logic-launcher.sql
+++ logic-launcher.sql
@@ -5,4 +5,4 @@ where e.department_id = departments.department_id and departments.name like "%En
 select first_name, last_name, position, salary, e.department_id from employees e , departments 
 where e.department_id = departments.department_id and departments.name not like "Analytics" and salary > 5000;
 select first_name, last_name, position, salary, department_id from employees 
-where salary < 5000 and hire_date like '2023%'; 
+where salary < 5000 and strftime('%Y', hire_date) = 2023; 

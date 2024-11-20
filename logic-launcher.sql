5b93b0d2e9eccb6d4c92901be028332c70cb7337
nkali <kalinurbah040508@gmail.com>
2024-11-20 13:19:30 +0500

    inner join removes

--- logic-launcher.sql
+++ logic-launcher.sql
@@ -1,10 +1,8 @@
-SELECT first_name, last_name, position, salary, e.department_id from employees e
+SELECT first_name, last_name, position, salary, department_id from employees 
 where salary > 5000 and position like '%Engineer%';
-select first_name, last_name, position, salary, e.department_id from employees e
-inner join departments on e.department_id = departments.department_id
-where departments.name like "Engineering" or departments.name like "Development";
-select first_name, last_name, position, salary, e.department_id from employees e
-inner join departments on e.department_id = departments.department_id
-where  departments.name not like "Analytics" and salary > 5000;
-select first_name, last_name, position, salary, e.department_id from employees e
+select first_name, last_name, position, salary, e.department_id from employees e, departments 
+where e.department_id = departments.department_id and departments.name like "%Engineering%" or departments.name like "%Development%";
+select first_name, last_name, position, salary, e.department_id from employees e , departments 
+where e.department_id = departments.department_id and departments.name not like "Analytics" and salary > 5000;
+select first_name, last_name, position, salary, department_id from employees 
 where salary < 5000 and hire_date like '2023%'; 

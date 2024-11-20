d9ec1b8d78aea1ffbb6a66dffb2b786407e5bf31
nkali <kalinurbah040508@gmail.com>
2024-11-20 15:25:18 +0500

    case lego

---
+++ case-constructor.sql
@@ -0,0 +1,11 @@
+select 
+first_name || ' ' || last_name as full_name,
+case 
+    when salary > 75000 then 'High'
+    when salary > 50000 then 'Medium'
+    else 'Low'
+end as 'Salary category',
+case
+    when department_id < 5 then 'Core Team'
+    else 'Support Team'
+end as 'Employee type' from employees;

5bd0de1cbe65335a0a6458bda7b48e01a19b7f52
nkali <kalinurbah040508@gmail.com>
2024-11-20 13:33:00 +0500

    between and in

---
+++ range-ranger.sql
@@ -0,0 +1,8 @@
+select first_name, last_name, position from employees 
+where salary between 45000 and 55000;
+select first_name, last_name, position from employees 
+where department_id in (1, 3);
+select first_name, last_name, position from employees 
+where hire_date  between '2014-01-01' and '2019-12-31';
+select first_name, last_name, position from employees 
+where position in ('Warehouse Manager', 'Senior Researcher', 'Lead Engineer');

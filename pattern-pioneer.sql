7c469fcfb84de1b46b663b38f7965ad041199abd
nkali <kalinurbah040508@gmail.com>
2024-11-20 13:45:45 +0500

    pattern like

--- pattern-pioneer.sql
+++ pattern-pioneer.sql
@@ -3,6 +3,6 @@ where first_name like 'M%';
 select first_name, last_name, position, email from employees
 where last_name like '%son';
 select first_name, last_name, position, email from employees
-where email like '.w';
+where email like '%.w%';
 select first_name, last_name, position, email from employees
 where position like 'Senior%';

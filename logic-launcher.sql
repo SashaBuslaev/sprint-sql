9349e8344976e29f8be462ab5c3c53ce86dd3250
nkali <kalinurbah040508@gmail.com>
2024-11-20 13:23:49 +0500

    what

--- logic-launcher.sql
+++ logic-launcher.sql
@@ -1,8 +1,11 @@
 SELECT first_name, last_name, position, salary, department_id from employees 
 where salary > 5000 and position like '%Engineer%';
+
 select first_name, last_name, position, salary, e.department_id from employees e, departments 
 where e.department_id = departments.department_id and departments.name like "Engineering" or departments.name like "Development";
+
 select first_name, last_name, position, salary, e.department_id from employees e , departments 
-where e.department_id = departments.department_id and departments.name not like "Analytics" and salary > 5000;
+where e.department_id = departments.department_id and departments.name like "Analytics" and salary > 5000;
+
 select first_name, last_name, position, salary, department_id from employees 
 where salary < 5000 and strftime('%Y', hire_date) = 2023; 

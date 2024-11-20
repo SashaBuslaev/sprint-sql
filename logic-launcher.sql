59bc2cd0e3b9db0a6ec545431f8c6db9c68e43a2
nkali <kalinurbah040508@gmail.com>
2024-11-20 13:17:18 +0500

    percent

--- logic-launcher.sql
+++ logic-launcher.sql
@@ -2,9 +2,9 @@ SELECT first_name, last_name, position, salary, e.department_id from employees e
 where salary > 5000 and position like '%Engineer%';
 select first_name, last_name, position, salary, e.department_id from employees e
 inner join departments on e.department_id = departments.department_id
-where departments.name like "%Engineering%" or departments.name like  "%Development%";
+where departments.name like "Engineering" or departments.name like "Development";
 select first_name, last_name, position, salary, e.department_id from employees e
 inner join departments on e.department_id = departments.department_id
-where  departments.name not like "%Analytics%" and salary > 5000;
+where  departments.name not like "Analytics" and salary > 5000;
 select first_name, last_name, position, salary, e.department_id from employees e
 where salary < 5000 and hire_date like '2023%'; 

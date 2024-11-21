36c4427eba4756d88317533ca6f21275cb438fba
nkali <kalinurbah040508@gmail.com>
2024-11-21 14:10:56 +0500

    test last

---
+++ salary-analysis.sql
@@ -0,0 +1,2 @@
+SELECT e.first_name, e.last_name, e.department_id, e.salary, d.name as department_name FROM departments d, employees e
+WHERE d.department_id = e.department_id and salary > (SELECT avg(salary) FROM employees GROUP BY department_id) and salary > (SELECT avg(salary) FROM employees) and e.department_id = (SELECT department_id from departments WHERE budget > (SELECT avg(budget) FROM departments));

e0ef1caeaa2ac8395552efc4cb5dfb6ea626d868
nkali <kalinurbah040508@gmail.com>
2024-11-21 14:24:38 +0500

    push push

--- salary-analysis.sql
+++ salary-analysis.sql
@@ -1,2 +1,5 @@
 SELECT e.first_name, e.last_name, e.department_id, e.salary, d.name as department_name FROM departments d, employees e
-WHERE d.department_id = e.department_id and salary > (SELECT avg(salary) FROM employees GROUP BY department_id) and salary > (SELECT avg(salary) FROM employees) and e.department_id = (SELECT department_id from departments WHERE budget > (SELECT avg(budget) FROM departments));
+WHERE d.department_id = e.department_id 
+and salary > (SELECT avg(salary) FROM employees GROUP BY department_id HAVING department_id = e.department_id)
+and salary > (SELECT avg(salary) FROM employees) 
+and d.budget > (SELECT avg(budget) FROM departments);

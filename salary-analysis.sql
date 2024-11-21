437bb8fe1307018696c6ab0cdb9640cc42b2dc4d
nkali <kalinurbah040508@gmail.com>
2024-11-21 14:25:34 +0500

    mustafa

--- salary-analysis.sql
+++ salary-analysis.sql
@@ -2,4 +2,5 @@ SELECT e.first_name, e.last_name, e.department_id, e.salary, d.name as departmen
 WHERE d.department_id = e.department_id 
 and salary > (SELECT avg(salary) FROM employees GROUP BY department_id HAVING department_id = e.department_id)
 and salary > (SELECT avg(salary) FROM employees) 
-and d.budget > (SELECT avg(budget) FROM departments);
+and d.budget > (SELECT avg(budget) FROM departments)
+order by salary desc;

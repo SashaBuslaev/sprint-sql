6992e5309d8cb03ac74989b03a3222b2f3508cf0
nkali <kalinurbah040508@gmail.com>
2024-11-25 17:21:46 +0500

    2pizza mista

--- join-juggling.sql
+++ join-juggling.sql
@@ -1,14 +1,14 @@
 SELECT 
 t.Name AS TrackName,
-al.Name AS AlbumTitle,
+al.Title AS AlbumTitle,
 ar.Name AS ArtistName, 
 g.Name AS GenreName,
 m.Name AS MediaTypeName
-FROM tracks
-JOIN albums USING (TrackId)
-JOIN artists USING (ArtistId)
-JOIN genres USING (GenreId)
-JOIN media_types USING (MediaTypeId)
+FROM tracks t
+JOIN albums al USING (AlbumId)
+JOIN artists ar USING (ArtistId)
+JOIN genres g USING (GenreId)
+JOIN media_types m USING (MediaTypeId)
 WHERE t.Milliseconds >= 600000 AND GenreName like 'Rock'
 ORDER BY GenreName;
 

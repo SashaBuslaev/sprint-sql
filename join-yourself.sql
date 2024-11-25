ab348386585aa6f8bd9db284f9e4dfad585abde2
nkali <kalinurbah040508@gmail.com>
2024-11-25 17:28:49 +0500

    serve

---
+++ join-yourself.sql
@@ -0,0 +1,6 @@
+SELECT FirstName AS ManagerFirstName, LastName AS ManagerLastName, Title AS ManagerTitle,
+(SELECT count(FirstName) FROM employees
+WHERE e.EmployeeId = ReportsTo
+) AS SubordinateCount
+FROM employees e
+ORDER BY 

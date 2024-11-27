8186d08aab556cb6973e52565560c1e6a6627a03
nkali <kalinurbah040508@gmail.com>
2024-11-27 15:33:05 +0500

    fix

--- maximum-safety.sql
+++ maximum-safety.sql
@@ -9,7 +9,7 @@ WHERE ArtistId in (SELECT ArtistId FROM albums GROUP BY ArtistId HAVING count(Al
 
 UPDATE tracks 
 SET UnitPrice = UnitPrice * 0.8
-WHERE AlbumId = (SELECT AlbumId FROM albums WHERE ArtistId = (SELECT ArtistId FROM artists WHERE Name = 'Acquired Records'));
+WHERE AlbumId in (SELECT AlbumId FROM albums WHERE ArtistId = (SELECT ArtistId FROM artists WHERE Name = 'Acquired Records'));
 
 DELETE FROM artists 
 WHERE ArtistId NOT IN (SELECT ArtistId FROM albums);

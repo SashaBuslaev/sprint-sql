7cd5214a321b6d0b4375ee1ff27eaf45c123d86f
nkali <kalinurbah040508@gmail.com>
2024-11-27 13:47:58 +0500

    cascase

--- cascade-clone.sql
+++ cascade-clone.sql
@@ -5,7 +5,7 @@ DELETE FROM invoice_items
 WHERE TrackId = (SELECT TrackId FROM tracks WHERE MediaTypeId = (SELECT MediaTypeId FROM media_types WHERE Name = 'MPEG audio file'))
 ;
 DELETE FROM tracks
-WHERE TrackId = (SELECT TrackId FROM tracks WHERE MediaTypeId = (SELECT MediaTypeId FROM media_types WHERE Name = 'MPEG audio file'))
+WHERE MediaTypeId = (SELECT MediaTypeId FROM media_types WHERE Name = 'MPEG audio file')
 ;
 DELETE FROM media_types
-WHERE Name like 'MPEG audio file';
+WHERE Name = 'MPEG audio file';

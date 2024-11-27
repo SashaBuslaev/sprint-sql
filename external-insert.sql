7d08ee3323b60e06663d32ff00c881fac5e7189d
nkali <kalinurbah040508@gmail.com>
2024-11-27 12:57:22 +0500

    new

--- external-insert.sql
+++ external-insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO playlist_track
-SELECT (SELECT PlaylistId FROM playlists WHERE Name like "Classical"), TrackId
-FROM playlist_track
-WHERE (SELECT PlaylistId FROM playlists WHERE Name like "Rock");
+SELECT (SELECT PlaylistId FROM playlists WHERE Name like "Classical"), pl.TrackId
+FROM playlist_track pl
+WHERE (SELECT PlaylistId FROM playlists WHERE Name like "Rock") = pl.PlaylistId and (SELECT Milliseconds FROM tracks WHERE TrackId = pl.TrackId) > 300000;

4a5646f34342a103f5710cf07cbc47b2afa5de0f
nkali <kalinurbah040508@gmail.com>
2024-11-27 13:00:51 +0500

    no gpt

--- external-insert.sql
+++ external-insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO playlist_track
-SELECT (SELECT PlaylistId FROM playlists WHERE Name like "Classical"), pl.TrackId
-FROM playlist_track pl
-WHERE (SELECT PlaylistId FROM playlists WHERE Name like "Rock") = pl.PlaylistId and (SELECT Milliseconds FROM tracks WHERE TrackId = pl.TrackId) > 300000;
+SELECT (SELECT PlaylistId FROM playlists WHERE Name like "Classical"), t.TrackId
+FROM tracks t
+WHERE (SELECT GenreId FROM genres WHERE Name like "Rock") = t.GenreId and Milliseconds > 300000;
